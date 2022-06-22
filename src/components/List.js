b6d817ff1be0069b97a7deb9914b280f5ab2d8cf
Carlos <carlos.guevara@ucrso.info>
2022-06-22 12:06:12 -0600

    4. New Dir and List Component

---
+++ src/components/List.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const List = (props) => {
+  const {contents} = props;
+
+  if(!contents || contents.length === 0) return <h1>No hay contenido</h1>
+
+  return (
+    <ul>
+      <h2 className= "Component-Title"> Estos son los contenidos disponibles</h2>
+      {contents.map((content) =>{
+        return (
+          <div style={{background: "yellow", margin: "10px"}}>
+              <li key={content.id}>
+                <span className="Component-Title">{content.id}<b>Nombre:</b> {content.name}</span>
+              </li>
+          </div>
+        );
+      })}
+    </ul>
+  )
+}
+export default List;
