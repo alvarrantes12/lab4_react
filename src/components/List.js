1649c92699578ce956f08579c4d210490abbc2be
Carlos <carlos.guevara@ucrso.info>
2022-06-22 13:11:03 -0600

    Step 4,5 and 6

--- src/components/List.js
+++ src/components/List.js
@@ -1,20 +1,33 @@
 import React from "react";
 
 const List = (props) => {
-  const {contents} = props;
+  const { contents } = props;
 
-  if(!contents || contents.length === 0) return <h1>No hay contenido</h1>
+  if (!contents || contents.length === 0) return <h1>No hay contenido</h1>
 
   return (
     <ul>
-      <h2 className= "Component-Title"> Estos son los contenidos disponibles</h2>
-      {contents.map((content) =>{
+      <h2 className="Component-Title"> Estos son los contenidos disponibles</h2>
+      {contents.map(({id, name, age, director: {first_name, last_name}}) => {
         return (
-          <div style={{background: "yellow", margin: "10px"}}>
-              <li key={content.id}>
-                <span className="Component-Title">{content.id}<b>Nombre:</b> {content.name}</span>
-              </li>
-          </div>
+          <table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Año de estreno</th>
+                <th>Director</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{id}</td>
+                <td>{name}</td>
+                <td>{age}</td>
+                <td><>{first_name}</> <>{last_name}</></td>
+              </tr>
+            </tbody>
+          </table>
         );
       })}
     </ul>
