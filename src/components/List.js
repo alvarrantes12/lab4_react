1a4053871723b12fd3b9014ea9dba0c52d3d72c7
NicoleGarciaLuna <nicolegarcia86@hotmail.com>
2022-06-22 15:36:36 -0600

    Step Edit, Delete, Error with Create

--- src/components/List.js
+++ src/components/List.js
@@ -12,10 +12,10 @@ const List = (props) => {
         return (
           <div style={{background: "yellow", margin: "10px"}}>
               <li key={content.id}> 
-                <span className="Component-Title">{content.id}<b>Id:</b> {content.id}</span>
-                <span className="Component-Title">{content.id}<b>Nombre Pelicula:</b> {content.name}</span>
-                <span className="Component-Title">{content.id}<b>Año de publicación:</b> {content.year_of_publication}</span>
-                <span className="Component-Title">{content.id}<b>Director:</b> {content.director.name}</span>
+                <span className="Component-Title"><b>Id:</b> {content.id}</span>
+                <span className="Component-Title"><b>   Nombre Pelicula:</b> {content.name}</span>
+                <span className="Component-Title"><b>   Año de publicación:</b> {content.year_of_publication}</span>
+                <span className="Component-Title"><b>   Director:</b> {content.director.name}</span>
               </li>
           </div>
         
